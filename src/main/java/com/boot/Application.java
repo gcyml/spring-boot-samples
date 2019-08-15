fd2aa6dbd99dbf6e12b783bb5c3df1ec454c692d
hjd <hjd1622@qq.com>
2019-08-15 14:05:52 +0800

    git测试

--- src/main/java/com/boot/Application.java
+++ src/main/java/com/boot/Application.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) throws Exception {
          SpringApplication.run(Application.class, args);
+
      }
 }
